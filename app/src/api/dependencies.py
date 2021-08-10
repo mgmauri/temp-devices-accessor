9e2176f8f0133395d1766525615292da81705926
Mauricio Garcia <maurigarcia88@gmail.com>
2021-08-10 17:21:23 -0300

    Update dependencies.py file

--- app/src/api/dependencies.py
+++ app/src/api/dependencies.py
@@ -12,3 +12,7 @@ def get_silicon_thermal_drivers_service() -> SiliconThermalDriversService:
 def get_gpio_output_drivers_service() -> GpioOutputDriversService:
     gpio_output_drivers_service = GpioDriversContainer.service()
     return gpio_output_drivers_service
+
+
+if __name__ == "__main__":
+    st_s = get_silicon_thermal_drivers_service()

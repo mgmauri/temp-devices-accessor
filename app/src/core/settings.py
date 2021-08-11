23e686226a5e0809309a73253bcf65488166dc60
Mauricio Garcia <maurigarcia88@gmail.com>
2021-08-11 16:09:59 +0100

    Update settings and simple config file

--- app/src/core/settings.py
+++ app/src/core/settings.py
@@ -8,5 +8,5 @@ class Settings(BaseSettings):
 
 
 # FIXME env_ setting
-env_ = os.environ["PROJ_TDM"] + "/.env"
+env_ = "/home/projects/temp-devices-manager/.env"
 settings = Settings(_env_file=env_)

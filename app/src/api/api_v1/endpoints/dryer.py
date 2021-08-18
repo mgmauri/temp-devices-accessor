ba95c753524d889811421b3fa67913491bc86b86
Mauricio Garcia <maurigarcia88@gmail.com>
2021-08-18 15:30:29 -0300

    Add gpio output pulse endpoint and methods

--- app/src/api/api_v1/endpoints/dryer.py
+++ app/src/api/api_v1/endpoints/dryer.py
@@ -1,11 +1,29 @@
-from typing import Any
+from typing import Any, Optional
 
 from fastapi import APIRouter, Depends
 from src.api.dependencies import get_gpio_output_drivers_service
 from src.api.exceptions import EvkNameNotFound
+from src.core.configs import OperationConfig
 from src.services.gpio_service import GpioOutputDriversService
 
 router = APIRouter()
+
+
+@router.post("/drivers/dryers/{evk_name}/air_blast")
+def set_negative_pulse_by_evk(
+    evk_name: str,
+    width: Optional[float] = OperationConfig.config.dryer_pulse_width(),
+    gpio_output_drivers_service: GpioOutputDriversService = Depends(
+        get_gpio_output_drivers_service
+    )
+) -> Any:
+    if gpio_output_drivers_service.is_valid_evk(evk_name):
+        return gpio_output_drivers_service.negative_pulse_by_evk(
+            evk_name,
+            width
+        )
+    else:
+        raise EvkNameNotFound
 
 
 @router.get("/drivers/dryers/{evk_name}/state")
--- app/src/drivers/gpio.py
+++ app/src/drivers/gpio.py
@@ -1,21 +1,32 @@
+from threading import Timer
+
+
 class GpioOutputDriver:
     def __init__(self, pin_number: int) -> None:
         self._pin_number = pin_number
-        self._state = None
+        self._value = None
 
     @property
     def value(self) -> bool:
         # code to read pin state or just return internal variable self._state
-        return self._state
+        return self._value
 
     @value.setter
     def value(self, value: bool):
         # code to set pin state
-        self._state = value
+        print(f"PIN{self._pin_number}={value}")
+        self._value = value
+
+    def set_value(self, value: bool) -> None:
+        self.value = value
+
+    def negative_pulse(self, width: float = 5) -> None:
+        Timer(width, self.set_value, args=(True,)).start()
+        self.value = False
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._pin_number})"
 
 
 if __name__ == "__main__":
-    relays_controller = GpioOutputDriver(pin_number=2)
+    gpio = GpioOutputDriver(pin_number=2)

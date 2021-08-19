073f132ba6777e7490873ce60de7d5e3c60fa1bb
Mauricio Garcia <maurigarcia88@gmail.com>
2021-08-19 19:44:58 -0300

    Add watchdog service and update config files accordingly

--- app/src/api/api_v1/endpoints/silicon_thermal.py
+++ app/src/api/api_v1/endpoints/silicon_thermal.py
@@ -2,27 +2,29 @@ from typing import Any
 
 from fastapi import APIRouter, Depends
 from fastapi.param_functions import Query
-from src.api.dependencies import get_silicon_thermal_drivers_service
+from src.api.dependencies import (get_silicon_thermal_drivers_service,
+                                  get_watchdog_service)
 from src.api.exceptions import EvkNameNotFound
 from src.core.configs import OperationConfig
 from src.services.silicon_thermal_service import SiliconThermalDriversService
+from src.services.watchdog_service import WatchdogService
 
 router = APIRouter()
 
 
-@router.put("/drivers/silicon_thermals/{evk_name}/target_temperature")
+@router.post("/drivers/silicon_thermals/{evk_name}/target_temperature")
 def set_target_temperature_by_evk(
     evk_name: str,
     temperature: float = Query(...,
                                gt=OperationConfig.config.minimum_temperature(),
                                lt=OperationConfig.config.maximum_temperature()
                                ),
-    silicon_thermal_drivers_service: SiliconThermalDriversService = Depends(
-        get_silicon_thermal_drivers_service
+    watchdog_service: WatchdogService = Depends(
+        get_watchdog_service
     )
 ) -> Any:
-    if silicon_thermal_drivers_service.is_valid_evk(evk_name):
-        return silicon_thermal_drivers_service.set_target_temperature_by_evk(
+    if watchdog_service.is_valid_evk(evk_name):
+        return watchdog_service.set_target_temperature_by_evk(
             evk_name, temperature
         )
     else:
---
+++ app/src/services/watchdog_service.py
@@ -0,0 +1,84 @@
+import time
+from threading import Timer
+
+from src.services.gpio_service import GpioOutputDriversService
+from src.services.silicon_thermal_service import SiliconThermalDriversService
+
+
+class WatchdogService:
+    def __init__(
+        self,
+        silicon_thermal_drivers_service: SiliconThermalDriversService,
+        gpio_output_drivers_service: GpioOutputDriversService,
+        watchdog_interval: float,
+        air_blast_duration: float,
+        temperature_threshold: float,
+        default_temperature: float
+    ) -> None:
+        self.silicon_thermal_drivers_service = silicon_thermal_drivers_service
+        self.gpio_output_drivers_service = gpio_output_drivers_service
+        self.watchdog_interval = watchdog_interval
+        self.air_blast_duration = air_blast_duration
+        self.temperature_threshold = temperature_threshold
+        self.default_temperature = default_temperature
+        self.timers_by_evk = {}
+
+    def is_valid_evk(self, evk_name: str) -> bool:
+        return self.silicon_thermal_drivers_service.is_valid_evk(evk_name)
+
+    def set_target_temperature_by_evk(
+        self, evk_name: str, temperature: float
+    ) -> None:
+        if temperature < self.temperature_threshold:
+            self.execute_at_under_threshold_temperature(evk_name, temperature)
+        else:
+            self.execute_at_above_threshold_temperature(evk_name, temperature)
+
+    def execute_at_under_threshold_temperature(
+        self, evk_name: str, temperature: float
+    ) -> None:
+        timer = self.timers_by_evk.get(evk_name, None)
+        if timer is None:
+            self.timers_by_evk[evk_name] = Timer(
+                interval=self.watchdog_interval,
+                function=self.kick,
+                args=(evk_name,)
+            )
+            self.timers_by_evk[evk_name].start()
+        self.silicon_thermal_drivers_service.set_target_temperature_by_evk(
+                evk_name, temperature
+        )
+
+    def execute_at_above_threshold_temperature(
+        self, evk_name: str, temperature: float
+    ) -> None:
+        target_temperature = self.silicon_thermal_drivers_service.\
+            get_target_temperature_by_evk(evk_name)
+        if target_temperature < 0:
+            self._close_timer(evk_name)
+            self.air_blast(evk_name)
+        self.silicon_thermal_drivers_service.set_target_temperature_by_evk(
+                evk_name, temperature
+            )
+
+    def air_blast(self, evk_name: str) -> None:
+        self.gpio_output_drivers_service.negative_pulse_by_evk(
+            evk_name, self.air_blast_duration
+        )
+        time.sleep(self.air_blast_duration)
+
+    def kick(self, evk_name: str):
+        print("kick!!!")
+        self._close_timer(evk_name)
+        self.air_blast(evk_name)
+        self.silicon_thermal_drivers_service.set_target_temperature_by_evk(
+                evk_name, self.default_temperature
+            )
+
+    def _close_timer(self, evk_name: str) -> None:
+        try:
+            self.timers_by_evk[evk_name].cancel()
+            self.timers_by_evk[evk_name] = None
+        except AttributeError:
+            # FIXME add logger!
+            print("Trying to close non existing timer")

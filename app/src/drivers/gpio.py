a97c49c6b18b57a670a14e0c0dcfaa56c2a11df0
Mauricio Garcia <maurigarcia88@gmail.com>
2021-11-04 16:54:09 -0300

    Use mocked drivers files to develop

--- app/src/drivers/gpio.py
+++ app/src/drivers/gpio.py
@@ -1,46 +1,34 @@
 from threading import Timer
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD)
 
 
 class GpioOutputDriver:
     def __init__(self, pin_number: int) -> None:
         self._pin_number = pin_number
         self._value = None
-        GPIO.setup(self._pin_number, GPIO.OUT)
-        GPIO.output(self._pin_number, True)
         self.timer = None
 
     @property
     def value(self) -> bool:
-        # FIXME check if it's necessary to read input
-        self._value = GPIO.input(self._pin_number)
+        # code to read pin state or just return internal variable self._state
         return self._value
 
     @value.setter
     def value(self, value: bool):
+        # code to set pin state
+        print(f"PIN{self._pin_number}={value}")
         self._value = value
-        GPIO.output(self._pin_number, value)
 
-    def set_true_value_by_timer(self) -> None:
+    def set_true_value(self) -> None:
         self.value = True
 
     def negative_pulse(self, duration: float = 5) -> None:
         # FIXME add overlaping condition
-        self.timer = Timer(duration, self.set_true_value_by_timer).start()
         self.value = False
+        self.timer = Timer(duration, self.set_true_value).start()
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._pin_number})"
 
 
 if __name__ == "__main__":
-    pin1 = GpioOutputDriver(pin_number=31)
-    pin2 = GpioOutputDriver(pin_number=33)
-    pin3 = GpioOutputDriver(pin_number=35)
-    pin4 = GpioOutputDriver(pin_number=37)
-    pin5 = GpioOutputDriver(pin_number=32)
-    pin6 = GpioOutputDriver(pin_number=36)
-    pin7 = GpioOutputDriver(pin_number=38)
-    pin8 = GpioOutputDriver(pin_number=40)
+    gpio = GpioOutputDriver(pin_number=2)
